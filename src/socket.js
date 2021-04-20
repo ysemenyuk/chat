1fa476172f1d411bcc113020c06083bc690798ed
ysemenyuk <invent@list.ru>
2021-04-21 00:42:24 +0300

    fix import

--- src/socket.js
+++ src/socket.js
@@ -1,13 +1,12 @@
 import { io } from 'socket.io-client';
 
-// import { channelsActions, messagesActions, connectActions } from './store/slices.js';
 import { channelsActions } from './components/channels/channelsSlice.js';
-import { messagesActions } from './components/chat/chatSlice.js';
+import { messagesActions } from './components/messages/messagesSlice.js';
 import { connectActions } from './store/connectSlice.js';
 
 export default (store) => {
   const socket = io({
-    timeout: 10000,
+    timeout: 1000,
   });
 
   socket.on('newMessage', (response) => {
